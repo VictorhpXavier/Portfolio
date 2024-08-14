69c42e5c7a760540384263a4ae4db1af105f90b7
VictorhpXavier <victorhpxavier.09@gmail.com>
2024-08-14 21:51:00 +0000

    .

--- client/src/components/projects.js
+++ client/src/components/projects.js
@@ -1,12 +1,22 @@
 // src/components/About.js
 import React from 'react';
-
+import styles from '../styles/Projects.module.css'
 function About() {
   return (
     <div>
-      <h1>Projects Page</h1>
-      <h1>About Page2</h1>
-      <p>This is the About Page!</p>
+        <section id={styles.ProjectsContainer}>
+            <div className={styles.VHXProjects}>
+                <p>Website</p>
+            </div>
+            <div className={styles.VHXProjects2}>
+                <p>PortFolio</p>
+            </div>
+            <div className={styles.VHXProjects3}>
+                <p>Sentiment analysis</p>
+
+            </div>
+
+        </section>
     </div>
   );
 }
