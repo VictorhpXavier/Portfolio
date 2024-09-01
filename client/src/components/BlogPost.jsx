28f839a8117f6e7cf6da01388b9efcbc55477410
VictorhpXavier <victorhpxavier.09@gmail.com>
2024-09-01 23:50:05 +0000

    Example?

--- client/src/components/BlogPost.jsx
+++ client/src/components/BlogPost.jsx
@@ -5,11 +5,15 @@ import styles from '../styles/BlogPost.module.css'
 
 const blogPosts = {
   AiChatBot: {
-    title: "How was <strong>V<span>H</span>X</strong> Ai Chat Bot Made?",
+    title: "How was VHX Ai Chat Bot Made?",
     WhatIs: "<h2>What is <strong>V<span>H</span>X</strong> Ai ChatBot?<h2>",
-    Paragraph: "<p> <strong>V<span>H</span>X</strong> Ai ChatBot as the name suggest is an <span>AI chat bot</span> where you can chat with. Its a great tool for Question answer and to get the best responses for any question. <br>On top of that it will give the fastest answer possible with its top notch technology.</p>",
+    Paragraph: "<p> <strong>V<span>H</span>X</strong> AI chatbot is a conversational agent powered by artificial intelligence. It allows users to engage in text-based interactions, providing answers, assistance, and a personalized experience. The chatbot leverages advanced technology to deliver prompt and accurate responses.</p>",
     HeaderImage: `${process.env.PUBLIC_URL}/BlogImages/tile.jpg`,
-    content: 'this is a test'
+    Why: "<h2>Why i build the <strong>V<span>H</span>X</strong> Ai chat bot?</h2>",
+    Reason: "<p>This chatbot was my first full-stack web application project. I embarked on this journey to gain hands-on experience with several important technologies and concepts, including JSON Web Tokens (JWT), APIs, machine learning, authentication systems, and HTTP request handling. The project served as an excellent opportunity to expand my technical skills and apply them in a real-world scenario.</p>",
+    Languages: "<h2>How It Was Built</h2> <p>The chatbot was developed using a range of technologies:</p> <ul>   <li><strong>HTML/CSS:</strong> Used for designing the user interface.</li>   <li><strong>JavaScript:</strong> Employed with Node.js and Express for handling backend logic, user authentication, and managing user data.</li>   <li><strong>Python:</strong> Utilized to build the machine learning model that powers the chatbot's responses.</li>   <li><strong>MySQL:</strong> Implemented for storing user data, chat logs, and bot interactions.</li> </ul><p>Node.js and Express played a crucial role in managing user sessions and authentication, while Python was used to develop the machine learning algorithms that enable the chatbot to understand and generate responses. MySQL ensured the efficient storage and retrieval of user data and interactions.</p> </p>",
+    Improvements: "<h2>Future Improvements</h2> <p>Looking ahead, there are several areas for potential enhancement:</p> <ul>   <li><strong>Improving AI Capabilities:</strong> Further training the machine learning model to handle more complex queries and provide more accurate responses.</li>   <li><strong>User Interface Enhancements:</strong> Upgrading the user interface to make it more intuitive and engaging.</li>   <li><strong>Additional Features:</strong> Adding functionalities such as multilingual support and integration with other platforms.</li> </ul>",
+    FinalStatment: "<h2>Conclusion</h2> <p>Building the AI chatbot was a rewarding experience that allowed me to apply and expand my knowledge of web development and machine learning.</p>"
   },
   sentimentAnalysis: {
     title: "My First Machine Learning Project",
@@ -123,6 +127,12 @@ export default function BlogPost() {
             <img src={post.HeaderImage} alt="" />
         </div>
         <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+        <div dangerouslySetInnerHTML={{ __html: post.Why }}></div>
+        <div dangerouslySetInnerHTML={{ __html: post.Reason }}></div>
+        <div dangerouslySetInnerHTML={{ __html: post.Languages }} className={styles.Languages}></div>
+        <div dangerouslySetInnerHTML={{ __html: post.Improvements }} className={styles.Improvements}></div>
+        <div dangerouslySetInnerHTML={{ __html: post.FinalStatment }} className={styles.FinalStatment} ></div>
+
       </div>
     </div>
     <div className={styles.readMore} ref={readMoreRef}>
